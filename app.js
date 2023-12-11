4307ab9a302ec633a6e9d7968f808ad3c4a89419
sp1r14ual <sp1r14ual@gmail.com>
2023-12-11 23:09:29 +0700

    insert and get db actions

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const { Client } = require('pg');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'warehouse',
+  password: 'password123',
+  port: 5432,
+});
+
+client.connect();
+
+app.get('/get', (req, res) => {
+  try {
+    const result = client
+      .query('SELECT * FROM items')
+      .then(data => res.json(data.rows));
+  } catch (error) {
+    console.error('Error fetching items:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.post('/insert', (req, res) => {
+  const newItem = req.body;
+  console.log(newItem);
+
+  const insertDataQuery =
+    'INSERT INTO items (name, vendor_code, location, quantity, weight, shelf_life, shipper) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *';
+
+  client.query(
+    insertDataQuery,
+    [
+      newItem.name,
+      newItem.vendorCode,
+      newItem.location,
+      newItem.quantity,
+      newItem.weight,
+      newItem.shelfLife,
+      newItem.shipper,
+    ],
+    err => {
+      if (err) {
+        console.error('Error inserting data:', err);
+        res.send('Error inserting data');
+      } else {
+        res.send('Data inserted successfully');
+      }
+    }
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
---
+++ script.js
@@ -0,0 +1,235 @@
+let items = [];
+
+function fetchItems() {
+  fetch('http://127.0.0.1:3000/get') // Adjust the endpoint based on your server setup
+    .then(response => response.json())
+    .then(data => {
+      items = data;
+      console.log(items);
+      renderItems(items);
+    })
+    .catch(error => console.error('Error fetching items:', error));
+}
+
+function renderItems(displayItems) {
+  const itemList = document.getElementById('items');
+  itemList.innerHTML = '';
+
+  displayItems.forEach(item => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `
+      <strong>${item.name}</strong> -
+      Артикул: ${item.vendor_code} -
+      Место на складе: ${item.location} -
+      Количество: ${item.quantity} -
+      Вес: ${item.weight} кг -
+      Срок хранения: ${item.shelf_life} дней -
+      Поставщик: ${item.shipper}
+    `;
+
+    const buttonsContainer = document.createElement('div');
+
+    const writeOffButton = createButton('Списать товар', () =>
+      writeOffItem(item.id)
+    );
+    const changeLocationButton = createButton('Изменить место на складе', () =>
+      changeLocation(item.id)
+    );
+    const changeQuantityButton = createButton('Изменить количество', () =>
+      changeQuantity(item.id)
+    );
+    const changeWeightButton = createButton('Изменить вес', () =>
+      changeWeight(item.id)
+    );
+    const changeShelfLifeButton = createButton('Изменить срок хранения', () =>
+      changeShelfLife(item.id)
+    );
+    const changeShipperButton = createButton('Изменить поставщика', () =>
+      changeShipper(item.id)
+    );
+    const changeVendorCodeButton = createButton('Изменить артикул', () =>
+      changeVendorCode(item.id)
+    );
+
+    buttonsContainer.appendChild(writeOffButton);
+    buttonsContainer.appendChild(changeLocationButton);
+    buttonsContainer.appendChild(changeQuantityButton);
+    buttonsContainer.appendChild(changeWeightButton);
+    buttonsContainer.appendChild(changeShelfLifeButton);
+    buttonsContainer.appendChild(changeShipperButton);
+    buttonsContainer.appendChild(changeVendorCodeButton);
+
+    listItem.appendChild(buttonsContainer);
+    itemList.appendChild(listItem);
+  });
+}
+
+function createButton(text, clickHandler) {
+  const button = document.createElement('button');
+  button.textContent = text;
+  button.addEventListener('click', clickHandler);
+  return button;
+}
+
+function writeOffItem(itemId) {
+  const reason = prompt('Введите причину списания товара');
+  if (reason !== null) {
+    const confirmWriteOff = confirm(
+      `Вы уверены, что хотите списать товар?\nПричина: ${reason}`
+    );
+    if (confirmWriteOff) {
+      deleteItem(itemId);
+    }
+  }
+}
+
+function deleteItem(itemId) {
+  items = items.filter(item => item.id !== itemId);
+  renderItems(items);
+}
+
+function changeLocation(itemId) {
+  const item = items.find(item => item.id === itemId);
+  const newLocation = prompt('Введите место на складе');
+  if (newLocation !== null) {
+    item.location = newLocation;
+    renderItems(items);
+  }
+}
+
+function changeQuantity(itemId) {
+  const item = items.find(item => item.id === itemId);
+  const newQuantity = prompt('Введите количество');
+  if (newQuantity !== null) {
+    item.quantity = parseInt(newQuantity) || 0;
+    renderItems(items);
+  }
+}
+
+function changeWeight(itemId) {
+  const item = items.find(item => item.id === itemId);
+  const newWeight = prompt('Введите вес');
+  if (newWeight !== null) {
+    item.weight = parseFloat(newWeight) || 0;
+    renderItems(items);
+  }
+}
+
+function changeShelfLife(itemId) {
+  const item = items.find(item => item.id === itemId);
+  const newShelfLife = prompt('Введите срок хранения');
+  if (newShelfLife !== null) {
+    item.shelfLife = parseInt(newShelfLife) || 0;
+    renderItems(items);
+  }
+}
+
+function changeShipper(itemId) {
+  const item = items.find(item => item.id === itemId);
+  const newShipper = prompt('Введите поставщика');
+  if (newShipper !== null) {
+    item.shipper = newShipper;
+    renderItems(items);
+  }
+}
+
+function changeVendorCode(itemId) {
+  const item = items.find(item => item.id === itemId);
+  const newVendorCode = prompt('Введите артикул');
+  if (newVendorCode !== null) {
+    item.vendorCode = newVendorCode;
+    renderItems(items);
+  }
+}
+
+function addItem() {
+  const itemName = document.getElementById('itemName').value;
+  const vendorCode = document.getElementById('vendorCode').value;
+  const quantity = parseInt(document.getElementById('quantity').value) || 0;
+  const weight = parseFloat(document.getElementById('weight').value) || 0;
+  const shelfLife = parseInt(document.getElementById('shelfLife').value) || 0;
+  const shipper = document.getElementById('shipper').value;
+  const location =
+    document.getElementById('location').value || 'Место по умолчанию';
+
+  const newItem = {
+    id: Date.now(),
+    name: itemName,
+    vendorCode: vendorCode,
+    location: location,
+    quantity: quantity,
+    weight: weight,
+    shelfLife: shelfLife,
+    shipper: shipper,
+  };
+
+  fetch('http://127.0.0.1:3000/insert', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newItem),
+  }).then(response => {
+    if (response.ok) {
+      alert('Success');
+    } else {
+      alert('Failed');
+    }
+  });
+
+  items.push(newItem);
+  renderItems(items);
+}
+
+function searchItems() {
+  const searchTerm = document.getElementById('searchTerm').value.toLowerCase();
+  const searchCriteria = document.getElementById('searchCriteria').value;
+
+  let filteredItems = items;
+
+  switch (searchCriteria) {
+    case 'itemName':
+      filteredItems = items.filter(item =>
+        item.name.toLowerCase().includes(searchTerm)
+      );
+      break;
+    case 'vendorCode':
+      filteredItems = items.filter(item =>
+        item.vendorCode.toLowerCase().includes(searchTerm)
+      );
+      break;
+    case 'quantity':
+      filteredItems = items.filter(item =>
+        item.quantity.toString().includes(searchTerm)
+      );
+      break;
+    case 'weight':
+      filteredItems = items.filter(item =>
+        item.weight.toString().includes(searchTerm)
+      );
+      break;
+    case 'shelfLife':
+      filteredItems = items.filter(item =>
+        item.shelfLife.toString().includes(searchTerm)
+      );
+      break;
+    case 'shipper':
+      filteredItems = items.filter(item =>
+        item.shipper.toLowerCase().includes(searchTerm)
+      );
+      break;
+    case 'location':
+      filteredItems = items.filter(item =>
+        item.location.toLowerCase().includes(searchTerm)
+      );
+      break;
+    default:
+      alert('Invalid search criteria');
+      break;
+  }
+
+  renderItems(filteredItems);
+}
+
+// renderItems(items);
+fetchItems();
